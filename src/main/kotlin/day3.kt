72d5524e31eccfe75f1b5c1183735dc6bbb3687a
Volodymyr Masliy <thebestvova@gmail.com>
2022-12-03 08:15:23 +0100

    day3

---
+++ src/main/kotlin/day3.kt
@@ -0,0 +1,45 @@
+// a - z -> 1 - 26
+// A - Z -> 27 - 52
+fun main() {
+    val sample1 = part1(readFileLines("day3/sample.txt"))
+    check(sample1 == 157)
+    val part1 = part1(readFileLines("day3/input.txt"))
+    println("Part 1: $part1")
+
+    val sample2 = part2(readFileLines("day3/sample.txt"))
+    check(sample2 == 70)
+    val part2 = part2(readFileLines("day3/input.txt"))
+    println("Part 2: $part2")
+}
+
+private fun part1(lines: List<String>): Int {
+    return lines.map {
+        val comp1: String = it.slice(0 until it.length / 2)
+        val comp2: String = it.slice(it.length / 2..it.lastIndex)
+        val comp1Priorities: Set<Int> = comp1.toList().map { it.toPriority() }.toSet()
+        val comp2Priorities: Set<Int> = comp2.toList().map { it.toPriority() }.toSet()
+        val unique = comp1Priorities.toMutableSet()
+        unique.retainAll(comp2Priorities)
+        unique.sum()
+    }
+        .sum()
+}
+
+private fun part2(lines: List<String>): Int {
+    var sum = 0
+    for (i in 2..lines.lastIndex step 3) {
+        val unique = lines[i - 2].toHashSet()
+        unique.retainAll(lines[i - 1].toSet())
+        unique.retainAll(lines[i].toSet())
+        sum += unique.sumOf { it.toPriority() }
+    }
+    return sum
+}
+
+fun Char.toPriority(): Int {
+    return if (this.isLowerCase()) {
+        this.code - 96
+    } else {
+        this.code - 38
+    }
+}
