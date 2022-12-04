c7474f72a12eceec14646d49bf787959c54e2099
Volodymyr Masliy <thebestvova@gmail.com>
2022-12-04 15:58:32 +0100

    day 4 p1

--- src/main/kotlin/day4.kt
+++ src/main/kotlin/day4.kt
@@ -1,17 +1,31 @@
 fun main() {
     val sample1 = part1(readFileLines("day4/sample.txt"))
-    check(sample1 == 100) { println(sample1) }
+    check(sample1 == 2) { println(sample1) }
     val part1 = part1(readFileLines("day4/input.txt"))
     println("Part 1: $part1")
 
     val sample2 = part2(readFileLines("day4/sample.txt"))
-    check(sample2 == 70) { println(sample2) }
+    check(sample2 == 5) { println(sample2) }
     val part2 = part2(readFileLines("day4/input.txt"))
     println("Part 2: $part2")
 }
 
 private fun part1(lines: List<String>): Int {
-    return 0
+    return lines
+        .map { it.split(",") }
+        .map {
+            val range1 = it[0].split("-").map { it.toInt() }
+            val range2 = it[1].split("-").map { it.toInt() }
+            if (
+                (range1[0] >= range2[0] && range1[1] <= range2[1])
+                || (range1[0] <= range2[0] && range1[1] >= range2[1])
+            ) {
+                1
+            } else {
+                0
+            }
+        }
+        .sum()
 }
 
 private fun part2(lines: List<String>): Int {
