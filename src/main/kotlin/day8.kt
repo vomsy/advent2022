a5e0d4bddcda1737f24c25669953e992a153ac5b
Volodymyr Masliy <thebestvova@gmail.com>
2022-12-08 15:30:54 +0100

    day8 p2 refactor

--- src/main/kotlin/day8.kt
+++ src/main/kotlin/day8.kt
@@ -1,52 +1,20 @@
 fun main() {
-//    val sample1 = part1(read2DimArray("day8/sample.txt"))
-//    check(sample1 == 21) { println(sample1) }
-//    val part1 = part1(read2DimArray("day8/input.txt"))
-//    println("Part 1: $part1")
-
     val sample2 = part2(read2DimArray("day8/sample.txt"))
     check(sample2 == 8) { println(sample2) }
     val part2 = part2(read2DimArray("day8/input.txt"))
     println("Part 2: $part2")
+    check(part2 == 252000)
 }
 
 private fun part2(grid: Array<IntArray>): Int {
     var max = 0
     for ((r, row) in grid.withIndex()) {
-        for ((c, curr) in row.withIndex()) {
-            //go left
-            var countLeft = 0
-            for (i in (r - 1) downTo 0) {
-                countLeft++
-                if (grid[i][c] >= curr) {
-                    break
-                }
-            }
+        for ((c, currVal) in row.withIndex()) {
+            val countLeft = countHorizontal(grid, c, currVal, (r-1) downTo 0)
+            val countRight = countHorizontal(grid, c, currVal, (r+1)..grid.lastIndex)
+            val countUp = countVertical(grid, r, currVal, (c-1) downTo 0)
+            val countDown = countVertical(grid, r, currVal, (c+1)..row.lastIndex)
 
-            var countRight = 0
-            for (i in (r + 1)..grid.lastIndex) {
-                countRight++
-                if (grid[i][c] >= curr) {
-                    break;
-                }
-            }
-
-            var countUp = 0
-            for (i in (c - 1) downTo 0) {
-                countUp++
-                if (grid[r][i] >= curr) {
-                    break
-                }
-            }
-
-            //go down
-            var countDown = 0
-            for (i in (c + 1)..row.lastIndex) {
-                countDown++
-                if (grid[r][i] >= curr) {
-                    break
-                }
-            }
             val newMax = countLeft * countRight * countDown * countUp
             if (newMax > max) {
                 max = newMax
@@ -54,4 +22,29 @@ private fun part2(grid: Array<IntArray>): Int {
         }
     }
     return max
+}
+
+
+
+
+fun countHorizontal(grid: Array<IntArray>, colIdx: Int, currValue: Int, intRange: IntProgression): Int {
+    var count = 0
+    for (i in intRange) {
+        count++
+        if (grid[i][colIdx] >= currValue) {
+            break
+        }
+    }
+    return count
+}
+
+fun countVertical(grid: Array<IntArray>, rowIdx: Int, currValue: Int, intRange: IntProgression): Int {
+    var count = 0
+    for (i in intRange) {
+        count++
+       if (grid[rowIdx][i] >= currValue) {
+           break
+       }
+    }
+    return count
 }
