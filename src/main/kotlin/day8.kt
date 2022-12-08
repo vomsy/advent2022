740152b2ea6c751d8f1a97361a7e2d106b374ac9
Volodymyr Masliy <thebestvova@gmail.com>
2022-12-08 11:38:27 +0100

    d8p1

---
+++ src/main/kotlin/day8.kt
@@ -0,0 +1,89 @@
+const val VV = 1;
+const val HV = 2;
+
+fun main() {
+    val sample1 = part1(read2DimArray("day8/sample.txt"))
+    check(sample1 == 21) { println(sample1) }
+    val part1 = part1(read2DimArray("day8/input.txt"))
+    println("Part 1: $part1")
+    check(part1 == 1560) { println(part1) }
+}
+
+private fun part1(grid: Array<IntArray>): Int {
+    val visibility = Array(grid.size) { IntArray(grid[0].size) }
+    for ((i, r) in grid.withIndex()) {
+        checkRow(visibility[i], r)
+    }
+
+    for (i in 0..grid[0].lastIndex) {
+        checkCol(visibility, grid, i)
+    }
+
+    var count = 0
+    for (row in visibility) {
+        for (num in row) {
+            if (num != 0) {
+                count++
+            }
+        }
+    }
+    return count
+}
+
+private fun part2(grid: Array<IntArray>): Int {
+    return 0
+}
+
+fun checkRow(visibility: IntArray, gridRow: IntArray) {
+    var max = gridRow[0]
+    for ((idx, num) in gridRow.withIndex()) {
+        if (idx == 0) {
+            visibility[idx] = HV
+            continue
+        }
+        if (num > max) {
+            visibility[idx] = HV
+            max = num
+        }
+    }
+
+    max = gridRow.last()
+    for (idx in gridRow.lastIndex downTo 0) {
+        if (idx == gridRow.lastIndex) {
+            visibility[idx] = HV
+            continue
+        }
+
+        if (gridRow[idx] > max) {
+            visibility[idx] = HV
+            max = gridRow[idx]
+        }
+    }
+}
+
+fun checkCol(visibility: Array<IntArray>, gridRow: Array<IntArray>, col: Int) {
+    var max = gridRow[0][col]
+    for (r in 0..gridRow.lastIndex) {
+        if (r == 0) {
+            visibility[r][col] = visibility[r][col] or VV
+            continue
+        }
+        if (gridRow[r][col] > max) {
+            visibility[r][col] = visibility[r][col] or VV
+            max = gridRow[r][col]
+        }
+    }
+
+    max = gridRow[gridRow.lastIndex][col]
+    for (r in gridRow.lastIndex downTo 0) {
+        if (r == gridRow.lastIndex) {
+            visibility[r][col] = visibility[r][col] or VV
+            continue
+        }
+        if (gridRow[r][col] > max) {
+            visibility[r][col] = visibility[r][col] or VV
+            max = gridRow[r][col]
+        }
+    }
+}
+
--- src/main/kotlin/util.kt
+++ src/main/kotlin/util.kt
@@ -1,9 +1,24 @@
 import java.io.File
 
 fun readFileLines(fileName: String): List<String> {
-  return File("src/main/resources", fileName).readLines()
+    return File("src/main/resources", fileName).readLines()
 }
 
 fun readFileAsStr(fileName: String): String {
-  return File("src/main/resources", fileName).readText()
+    return File("src/main/resources", fileName).readText()
+}
+
+fun read2DimArray(fileName: String): Array<IntArray> {
+    val lines = readFileLines(fileName)
+    val rows = lines.size
+    val cols = lines[0].length
+    val array = Array(rows) { IntArray(cols) }
+
+    for ((row, line) in lines.withIndex()) {
+        for ((col, n) in line.withIndex()) {
+            array[row][col] = n.digitToInt()
+        }
+    }
+
+    return array
 }
