0e2217a6376ee3ddd7e2e1b56eb4105e118da545
Volodymyr Masliy <thebestvova@gmail.com>
2022-12-10 16:47:32 +0100

    day10 p2

--- src/main/kotlin/day10.kt
+++ src/main/kotlin/day10.kt
@@ -7,12 +7,62 @@ fun main() {
     println("Part 1: $part1")
     want(part1, 13060)
 
-    val sample2 = part2(readFileLines("day10/sample.txt"))
-    want(sample2, 1)
+    val sample2 = part2(readFileLines("day10/larger.txt"))
+    val expectedCrt = "##..##..##..##..##..##..##..##..##..##..\n" +
+            "###...###...###...###...###...###...###.\n" +
+            "####....####....####....####....####....\n" +
+            "#####.....#####.....#####.....#####.....\n" +
+            "######......######......######......####\n" +
+            "#######.......#######.......#######....."
+
+    want(sample2, expectedCrt)
     val part2 = part2(readFileLines("day10/input.txt"))
-    println("Part 2: $part2")
-    want(part2, 2793)
+    println("$part2")
+
+    want(
+        part2, "####...##.#..#.###..#..#.#....###..####.\n" +
+                "#.......#.#..#.#..#.#..#.#....#..#....#.\n" +
+                "###.....#.#..#.###..#..#.#....#..#...#..\n" +
+                "#.......#.#..#.#..#.#..#.#....###...#...\n" +
+                "#....#..#.#..#.#..#.#..#.#....#.#..#....\n" +
+                "#.....##...##..###...##..####.#..#.####."
+    )
+
 }
+
+private fun part2(lines: List<String>): String {
+    var curValue = 1
+    val cycles = mutableListOf<Int>()
+    for (line in lines) {
+        val split = line.split(" ")
+        val instruction = split[0]
+        if (instruction == "noop") {
+            cycles.add(curValue)
+        } else {
+            val addValue = split[1].toInt()
+            cycles.add(curValue)
+            cycles.add(curValue)
+            curValue += addValue
+        }
+    }
+
+    var crt = ""
+    for ((c, regVal) in cycles.withIndex()) {
+        if (c in 40..240 step 40) {
+            crt += "\n"
+        }
+        val spritePos = (regVal - 1)..(regVal + 1)
+
+        crt += if ((c % 40) in spritePos) {
+            "#"
+        } else {
+            "."
+        }
+    }
+
+    return crt;
+}
+
 
 private fun part1(lines: List<String>): Int {
     var curValue = 1
@@ -22,7 +72,7 @@ private fun part1(lines: List<String>): Int {
         val instruction = split[0]
         if (instruction == "noop") {
             cycles.add(curValue)
-        } else{
+        } else {
             val addValue = split[1].toInt()
             cycles.add(curValue)
             cycles.add(curValue)
@@ -33,13 +83,8 @@ private fun part1(lines: List<String>): Int {
     var result = 0
     for (i in 19..219 step 40) {
         val sigStrength = cycles[i] * (i + 1)
-        println("cycle ${i+1} * ${cycles[i]} = $sigStrength")
+//        println("cycle ${i + 1} * ${cycles[i]} = $sigStrength")
         result += sigStrength
     }
     return result
-}
-
-
-private fun part2(readFileLines: List<String>): Int {
-    return 0
 }
